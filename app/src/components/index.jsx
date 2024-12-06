5f62b99fdd8de9b73eb44d3daa1c0c3971ee1d5b
atahhabibi <habibiatah@gmail.com>
2024-12-05 23:13:15 -0800

    first face

--- app/src/components/index.jsx
+++ app/src/components/index.jsx
@@ -1,4 +1,4 @@
-import FormInput from "./formInput";
+import FormInput from "./FormInput";
 import SubmitBtn from "./SubmitBtn";
 import Navbar from "./Navbar";
 import Header from "./Header";
@@ -6,18 +6,17 @@ import ClockInOut from "./ClockInOut";
 import VolunteerTable from "./VolunteerTable";
 import EventOverviewTable from "./EventOverviewTable";
 import TaskOverviewTable from "./TaskOverviewTable";
-import TopPerformingVolunteersTable from './TopPerformingVolunteersTable';
-import PointsDistributionTable from './PointsDistributionTable';
-import { Pagination } from 'swiper/modules';
-import ProfileDataTable from './ProfileDataTable';
+import TopPerformingVolunteersTable from "./TopPerformingVolunteersTable";
+import PointsDistributionTable from "./PointsDistributionTable";
+import { Pagination } from "swiper/modules";
+import ProfileDataTable from "./ProfileDataTable";
 import TaskTable from "./TaskFormTable";
-import EventFormTable from './EventFormTable';
-import AdminPagination from './AdminPagination';
-import DeleteConfirmationModal from './DeleteConfirmationModal/';
-import PageError from './PageError';
-import PageLoading from './PageLoading';
-import ThemeToggle from './ThemeToggle';
-
+import EventFormTable from "./EventFormTable";
+import AdminPagination from "./AdminPagination";
+import DeleteConfirmationModal from "./DeleteConfirmationModal";
+import PageError from "./PageError";
+import PageLoading from "./PageLoading";
+import ThemeToggle from "./ThemeToggle";
 
 export {
   FormInput,
@@ -39,6 +38,4 @@ export {
   PageError,
   PageLoading,
   ThemeToggle
-
-
 };
